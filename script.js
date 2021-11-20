79a3c11ddc9081df2c46c2a4ff6db11ee7a64029
Felipe <adfelipealmeida@gmail.com>
2021-11-20 17:02:22 -0300

    feat: check if the value being passed is equal to the one you will withdraw

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ let amountOfNotes = [
   { amount: 1, value_note: 10 },
   { amount: 3, value_note: 5 },
   { amount: 12, value_note: 2 },
-  { amount: 10, value_note: 1 },
+  { amount: 1, value_note: 1 },
 ];
 
 const outOfNotes = (value) => {
   let current_value = document.getElementById("current_value");
   let response = [];
+  let fail_message = "";
 
   for (i = 0; i < amountOfNotes.length; i++) {
     let amount = amountOfNotes[i].amount;
@@ -21,19 +22,24 @@ const outOfNotes = (value) => {
 
     let result = Math.floor(value / value_note);
 
-    if (result && amount > 0) {
+    let enought_notes = amount - result;
+
+    if (result && enought_notes >= 0) {
       available = amount - result;
 
       current_value = result * value_note;
 
-      amountOfNotes[i].amount -= result;
+      amountOfNotes[i].amount = amount - result;
 
-      response.push(`${result} nota de R${value_note},00 `);
+      response.push(`${result} nota de R$ ${value_note},00 `);
 
       value -= current_value;
     }
   }
 
+  if (value > 0 || response === []) {
+    return (fail_message = `Não é possível realizar o saque falta de notas, por favor tente outro valor`);
+  }
   return response;
 };
 
@@ -47,21 +53,29 @@ button.addEventListener("click", () => {
   let result =
     Number.parseFloat(current_number) - Number.parseFloat(input_value);
 
-  if (result >= 0) {
-    account_balance.innerText = result.toFixed(2);
-    let message = outOfNotes(input_value);
-
-    if (message.length > 1) {
-      message.push("e");
-
-      last_position = message.splice(message.length - 2, 1);
-      message.push(last_position[0]);
-
-      current.innerHTML = message.join(" ");
-    } else {
-      current.innerText = message;
-    }
-  } else {
-    current.innerText = "Você não possui saldo para fazer esse saque";
+  if (result < 0) {
+    return (current.innerText = "Você não possui saldo para fazer esse saque");
   }
+
+  let message = outOfNotes(input_value);
+
+  if (typeof message === "string") {
+    return (current.innerText = message);
+  }
+
+  account_balance.innerText = result.toFixed(2);
+
+  if (message.length > 1) {
+    message.push("e");
+    last_position = message.splice(message.length - 2, 1);
+    message.push(last_position[0]);
+
+    current.innerHTML = message.join(" ");
+  } else {
+    current.innerText = message;
+  }
+
+  // else {
+  //   current.innerText = "Você não possui saldo para fazer esse saque";
+  // }
 });
